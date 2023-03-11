bbf97815f6267598e461ceb42584137abf4a8cb4
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2023-03-11 12:42:33 +0200

    add logger(morgan)

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+//for env file
 dotenv.config({ path: 'config.env' });
+
 const app = express();
+
+//apply  morgan middleware on development mode to see request state
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+  console.log(`mode:${process.env.NODE_ENV}`);
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World');
