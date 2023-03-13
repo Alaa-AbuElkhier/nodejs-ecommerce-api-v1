ab0be71ba13cede6bafd6102bac8ceb975b24f70
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2023-03-13 07:28:16 +0200

    refactor global error

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 //for env file
 dotenv.config({ path: 'config.env' });
 const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
 const dbConnection = require('./config/db');
 const categoryRoute = require('./routes/categoryRoute');
 
@@ -23,28 +24,14 @@ if (process.env.NODE_ENV == 'development') {
 
 //Mount Routes
 app.use('/api/v1/categories', categoryRoute);
+
 //Handling Routes error if we hit invalid route
 app.all('*', (req, res, next) => {
-  //first way
-  //Create error and send it to error handling middleware
-  // const err = new Error(`Cant find this route : ${req.originalUrl}`);
-  // next(err.message);
-  //second way
-  // next(new class('message', statusCode));
   next(new ApiError(`Cant find this route : ${req.originalUrl}`, 400));
 });
 
 //Global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-});
+app.use(globalError);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
