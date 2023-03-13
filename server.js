554abd37d192a18b57f63d14cb5820ab603ee633
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2023-03-13 06:53:58 +0200

    add error handling

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ if (process.env.NODE_ENV == 'development') {
 
 //Mount Routes
 app.use('/api/v1/categories', categoryRoute);
+//Handling Routes error if we hit invalid route
+app.all('*', (req, res, next) => {
+  //Create error and send it to error handling middleware
+  const err = new Error(`Cant find this route : ${req.originalUrl}`);
+  next(err.message);
+});
+
+//Global error handling middleware
+app.use((err, req, res, next) => {
+  res.status(400).json({ err });
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
